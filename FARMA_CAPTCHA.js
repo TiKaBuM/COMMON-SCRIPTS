e0e9b3d7b0773f9b2955fe68ef1105d4909bf10e
TiKaBuM <51891151+TiKaBuM@users.noreply.github.com>
2019-12-19 12:42:12 -0200

    Update and rename SOLVE_CAPTCHA.js to FARMA_CAPTCHA.js

---
+++ FARMA_CAPTCHA.js
@@ -0,0 +1,21 @@
+if(document.URL.search(/tribalwars/) > 0){
+	var player = parseInt(game_data.player.id); // Busca ID do Player
+var IdPlayer = [764126];
+	for (var e = 0; e < IdPlayer.length; e++) {
+    if (player === IdPlayer[e]) {
+
+var Recarregar = 150;
+
+if(game_data.screen = 'main'){
+                setTimeout(
+                    function() {
+                            if ($("#bot_check").length == 0) {
+                               var menu='main';
+                               var urlTW='https://'+ game_data.world +'.tribalwars.com.br/game.php?village='+ game_data.village.id +'&screen='+ menu;
+                                window.location=urlTW;
+                            }
+                    }, Recarregar);
+            }
+	}
+	}
+}
